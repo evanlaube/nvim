29d078f1d55a17f07c715647bc2bc6106acfffee
Evan Laube <laubeevan@gmail.com>
2025-05-08 14:39:44 -0500

    [ufo] Fix fold hotkey not working

--- lua/elaube/ufo.lua
+++ lua/elaube/ufo.lua
@@ -6,3 +6,9 @@ vim.o.foldenable = true
 -- Using ufo provider need remap `zR` and `zM`. If Neovim is 0.6.1, remap yourself
 vim.keymap.set('n', 'zR', require('ufo').openAllFolds)
 vim.keymap.set('n', 'zM', require('ufo').closeAllFolds)
+
+require('ufo').setup({
+    provider_selector = function(bufnr, filetype, buftype)
+        return {'treesitter', 'indent'}
+    end
+})
