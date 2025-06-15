c036232210506b26aea4030a99ab619e0fca7135
Evan Laube <laubeevan@gmail.com>
2025-06-15 13:53:17 -0500

    [color] Support transparent bg

--- lua/plugins/toykonight.lua
+++ lua/plugins/toykonight.lua
@@ -4,5 +4,8 @@ return {
         name = 'tokyonight',
         config = function()
             vim.cmd('colorscheme tokyonight-night')
+            -- Comment out to keep solid background
+            vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+            vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
         end
 }
