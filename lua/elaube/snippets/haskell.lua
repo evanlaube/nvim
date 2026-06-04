7ec31b203d56ef934077782f41714d34a9dc4fd0
Evan Laube <laubeevan@gmail.com>
2026-06-04 00:41:59 -0500

    Leftover updates - might want to revert

---
+++ lua/elaube/snippets/haskell.lua
@@ -0,0 +1,25 @@
+
+local ls = require('luasnip')
+local s = ls.snippet
+local t = ls.text_node
+local i = ls.insert_node
+local f = ls.function_node
+
+ls.add_snippets("haskell", {
+    s({ trig = "cbar", regTrig = false }, {
+        f(function(_, snip)
+            local line = vim.api.nvim_get_current_line()
+            local indent = line:match('^%s*')
+            local indent_width = vim.fn.strdisplaywidth(indent)
+            local bar_length = math.max(0, 76 - indent_width)
+            snip.bar_length = bar_length  -- Store for later use
+            return "{-" .. string.rep("-", bar_length)
+        end),
+        t({ "", "" }),
+        t({ "", "" }),
+        f(function(_, snip)
+            local bar_length = snip.bar_length or 71  -- Fallback to default
+            return string.rep("-", bar_length) .. "-}"
+        end),
+    }),
+})
---
+++ lua/plugins/tabout.lua
@@ -0,0 +1,27 @@
+return {}
+
+-- return {
+--   {
+--     'abecodes/tabout.nvim',
+--     lazy = true,
+--     config = function()
+--         require("elaube.tabout")
+--     end,
+--     dependencies = { -- These are optional
+--       "nvim-treesitter/nvim-treesitter",
+--       "L3MON4D3/LuaSnip",
+--       "hrsh7th/nvim-cmp"
+--     },
+--     after = { "nvim-treesitter", "LuaSnip", "nvim-cmp" },
+--     opt = true,  -- Set this to true if the plugin is optional
+--     event = 'InsertCharPre', -- Set the event to 'InsertCharPre' for better compatibility
+--     priority = 1000,
+--   },
+--   {
+--     "L3MON4D3/LuaSnip",
+--     keys = function()
+--       -- Disable default tab keybinding in LuaSnip
+--       return {}
+--     end,
+--   },
+-- }
